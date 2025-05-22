afe47bc4320e80afa081ecab9ce5778208a94df2
kamsycodes <preciousejikeme50@gmail.com>
2025-05-21 23:08:59 -0500

    updated main function

---
+++ module_1/main.py
@@ -0,0 +1,25 @@
+# print("Hello World")
+
+# print("This is my first summer 2025 project")
+
+def nameIs():
+  print("My Name is Kamsy")
+
+# nameIs()
+
+
+def greet(name):
+  print(f"Hello, {name}")
+
+# greet('Uche')
+
+# print("This is my first summer 2025 project")
+
+
+def main():
+    nameIs() 
+    greet("Uche")
+
+if __name__ == "__main__":
+    main()
+    
