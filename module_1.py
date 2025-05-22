4a185ec9d7b300396eda3ee4beb3bcc46373fc77
Precious Kamsiyo Ejikeme <31521713+kamsycodes@users.noreply.github.com>
2025-05-21 21:32:45 -0500

    Create module_1.py

---
+++ module_1.py
@@ -0,0 +1,13 @@
+Print("Hello World")
+
+Print("This is my first summer 2025 project")
+
+def nameIs():
+  print("My Name is Kamsy")
+
+nameIs
+
+def greet(name):
+  print(f"Hello, {name}")
+
+greet('Uche')
